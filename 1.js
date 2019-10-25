d2144a3d3ccb85f326906bcc06bd534b7dcd9130
Tran Hai Bao <tranhaibao1997@gmail.com>
2019-10-25 14:23:17 +0700

    adding my files to my repository

---
+++ 1.js
@@ -0,0 +1,24 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+        AOS.init();
+        var button = document.getElementsByClassName("open-menu-button");
+        var button = button[0];
+        var content = document.getElementsByClassName("noidungto");
+        var content = content[0];
+        var closebtn = document.getElementsByClassName("close-menu-btn");
+        var closebtn = closebtn[0];
+
+        button.onclick = function() {
+            content.classList.toggle("vetrai");
+        }
+
+
+        closebtn.onclick = function() {
+            content.classList.add("vetrai");
+        }
+
+
+
+    }
+
+
+    , false)
